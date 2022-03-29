ccd4db542a94eb3d1c0330f28ee8072654585b46
14rty <mail.14rty@gmail.com>
2022-03-29 22:33:27 +0300

    lab 1 done

--- webLab1/app/app.py
+++ webLab1/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,14 @@ application = app
 @app.route('/')
 
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/posts')
+
+def posts():
+    msg = 'Hello'
+    return render_template('posts.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
